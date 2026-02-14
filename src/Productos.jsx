2121d0da9f293655524a9deae403539d81fbcdb3
ChrisSR0 <Chriissr777@gmail.com>
2026-02-14 15:55:29 -0500

    Ejercicio 5: Agregar campo stock y mostrar estado (Disponible/Agotado) con ternario

--- src/Productos.jsx
+++ src/Productos.jsx
@@ -8,22 +8,26 @@ const products = [
     {
         id: "001",
         nombre: "Portátil Lenovo",
-        precio: 3250000
+        precio: 3250000,
+        stock : 3
     },
     {
         id: "002",
         nombre: "Monitor Samsung",
-        precio: 1200000
+        precio: 1200000,
+        stock : 6
     },
     {
         id: "003",
         nombre: "Teclado Mecánico",
-        precio: 250000
+        precio: 250000,
+        stock : 0
     },
     {
         id: "004",
         nombre: "Mouse Inalámbrico",
-        precio: 150000
+        precio: 150000,
+        stock : 11
     }
 ]
 
@@ -36,6 +40,11 @@ return(
                 <article key={product.id} className={styles.tarjeta}>
                     <h3 className={styles.titulo}>{product.nombre}</h3>
                     <p className={styles.precio}>${product.precio} COP</p>
+
+                    {/*Ejercicio #5  ternario para el stock*/}
+                    <p className={styles.estado}>
+                        {product.stock > 0 ? "Disponible" : "Agotado"}
+                    </p>
                 </article>
             )
         })}

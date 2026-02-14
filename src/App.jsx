7f01dd5a9496ec1c7065acdfddfb3fa02e1f7130
ChrisSR0 <Chriissr777@gmail.com>
2026-02-14 18:02:27 -0500

    Ejercicio 9: Mostrar notificaciones con map y condicional

--- src/App.jsx
+++ src/App.jsx
@@ -3,37 +3,57 @@ import CalcularEdad from "./CalcularEdad"
 import MensajeEstado from "./MensajeEstado"
 import Productos from "./Productos"
 import SaludoUsuario from "./SaludoUsuario"
-
 import Perfil from "./Perfil"
+import MostrarNotificaciones from "./MostrarNotificaciones"
+import './App.css'
 
 function App() {
 
 
   // app reenderiza el componente y react crea el obj props => luego Ejercicio1 recibe los valores del obj props y se muestra en pantalla la frase
   return (
-    <>
-      <Ejercicio1  ciudad="Popayan" nombre="Camilo" anio="2026"></Ejercicio1>
+    <div className="app-container">
+      <div className="ejercicio">
+        <h2>Ejercicio 1</h2>
+        <Ejercicio1  ciudad="Popayan" nombre="Camilo" anio="2026"></Ejercicio1>
+      </div>
 
-      <CalcularEdad anioNacimiento={2002}></CalcularEdad>
-      <CalcularEdad anioNacimiento={1984}></CalcularEdad>
-      <CalcularEdad anioNacimiento={1997}></CalcularEdad>
+      <div className="ejercicio">
+        <h2>Ejercicio 2 - Calcular Edad</h2>
+        <CalcularEdad anioNacimiento={2002}></CalcularEdad>
+        <CalcularEdad anioNacimiento={1984}></CalcularEdad>
+        <CalcularEdad anioNacimiento={1997}></CalcularEdad>
+      </div>
 
+      <div className="ejercicio">
+        <h2>Ejercicio 3 - Mensaje Estado</h2>
+        <MensajeEstado activo={true}></MensajeEstado>
+        <MensajeEstado activo={false}></MensajeEstado>
+      </div>
 
-      <MensajeEstado activo={true}></MensajeEstado>
-      <MensajeEstado activo={false}></MensajeEstado>
+      <div className="ejercicio">
+        <h2>Ejercicicio 4 y 5 - Productos</h2>
+        <Productos></Productos>
+      </div>
 
+      <div className="ejercicio">
+        <h2>Ejercicio 6 - Saludo Usuario</h2>
+        <SaludoUsuario nombre="Lucas" mensaje="Hola"></SaludoUsuario>
+        <SaludoUsuario  mensaje="Hola" nombre="Combinacion 2"></SaludoUsuario>
+        <SaludoUsuario  mensaje="Hola" nombre="Combinacion #3"></SaludoUsuario>
+      </div>
 
-      <Productos></Productos>
+      <div className="ejercicio">
+        <h2>Ejercicio 7 y 8 - Perfil</h2>
+        <Perfil nombre="Luis" edad="23" ciudad="Cali"></Perfil>
+        <Perfil nombre="Luis" edad="23" ></Perfil>
+      </div>
 
-
-      <SaludoUsuario nombre="Lucas" mensaje="Hola"></SaludoUsuario>
-      <SaludoUsuario  mensaje="Hola" nombre="Combinacion 2"></SaludoUsuario>
-      <SaludoUsuario  mensaje="Hola" nombre="Combinacion #3"></SaludoUsuario>
-
-
-      <Perfil nombre="Luis" edad="23" ciudad="Cali"></Perfil>
-      <Perfil nombre="Luis" edad="23" ></Perfil>
-    </>
+      <div className="ejercicio">
+        <h2>Ejercicio 9 Mostrar notificaciones</h2>
+          <MostrarNotificaciones></MostrarNotificaciones>
+      </div>
+    </div>
   )
 }
 
---
+++ src/MostrarNotificaciones.jsx
@@ -0,0 +1,31 @@
+// 9. Cree un arreglo notificaciones (strings) y una variable mostrarNotificaciones.
+// Si mostrarNotificaciones es true y el arreglo tiene elementos, renderice la
+// lista con map. Si no, muestre “No hay notificaciones”. Combine && y
+// condicional.
+
+
+const notificacionesArray = [
+    "Tienes un nuevo mensaje",
+    "Tu pedido ha sido enviado",
+    "Recordatorio: Reunión a las 3 PM",
+    "Nueva actualización disponible"
+]
+function MostrarNotificaciones (){
+
+    let verNotificacion = true
+
+    return (
+        <>
+            <h4>{verNotificacion && notificacionesArray.length > 0 ? 
+            
+            notificacionesArray.map((notificacion , i)=>(
+
+                <p key={i}> {notificacion}</p>
+            )): "!No hay notificaciones!"
+        
+            }</h4>
+        </>
+    )
+} 
+
+export default MostrarNotificaciones
